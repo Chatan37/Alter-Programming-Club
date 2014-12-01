ef2c37b6c2990ce954cdc1b133f83f8cb38835ad
Paul Jasek <papj2000@gmail.com>
2014-12-01 16:16:01 -0500

    New Stuff

--- Penguin/penguinGame.js
+++ Penguin/penguinGame.js
@@ -1,3 +1,12 @@
-(function() {	
+(function() {
+
+	var canvas = document.querySelector("canvas");
+	var drawingSurface = canvas.getContext("2d");
+			
+	var image = new Image();
+	image.src = "images/penguin.png";
+	image.addEventListener("load", function() {			
+		drawingSurface.drawImage(image, 50, 50, 300, 300);
+	}, false);
 	
 } ());
